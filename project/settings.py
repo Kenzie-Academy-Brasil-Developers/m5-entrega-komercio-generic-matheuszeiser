421a3914c1747259ba8c7114f97e41d6d6b95326
matheuszeiser <matheuszaizer@hotmail.com>
2022-10-27 00:35:04 -0300

    Commit heroku

--- project/settings.py
+++ project/settings.py
@@ -32,12 +32,11 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
+
 ALLOWED_HOSTS = [
     "komercio-matheuszeiser.herokuapp.com",
     "localhost",
 ]
-
-
 # Application definition
 
 DJANGO_APPS = [
